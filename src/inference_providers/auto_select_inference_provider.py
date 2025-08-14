666d5549fd8f080ea7fae5279a7726474461294e
StefKas1 <stefan.kasperzack@gmail.com>
2025-08-14 08:54:07 +0200

    Added inference provider.

---
+++ src/inference_providers/auto_select_inference_provider.py
@@ -0,0 +1,13 @@
+from huggingface_hub import InferenceClient
+
+
+client = (
+    InferenceClient()
+)  # Automatically handles inference provider selection and request routing
+
+completion = client.chat.completions.create(
+    model="deepseek-ai/DeepSeek-V3-0324",
+    messages=[{"role": "user", "content": "How many 'G's in 'huggingface'?"}],
+)
+
+print(completion.choices[0].message)
