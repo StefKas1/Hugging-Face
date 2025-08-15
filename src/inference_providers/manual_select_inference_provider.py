5504c9bf7294625b63cd660395e1c2f0cdc499ca
StefKas1 <stefan.kasperzack@gmail.com>
2025-08-15 09:10:32 +0200

    Added manual selection of inf. provider.

---
+++ src/inference_providers/manual_select_inference_provider.py
@@ -0,0 +1,23 @@
+import os
+from huggingface_hub import InferenceClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+client = InferenceClient(
+    provider="cerebras",
+    api_key=os.environ["HF_TOKEN"],
+)
+
+completion = client.chat.completions.create(
+    model="openai/gpt-oss-120b",
+    messages=[
+        {
+            "role": "user",
+            "content": "What is the capital of France?"
+        }
+    ],
+)
+
+print(completion.choices[0].message)
