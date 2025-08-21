4c6c6e4fa7b43a11f6468bfedc72cd22fbcaae6a
StefKas1 <stefan.kasperzack@gmail.com>
2025-08-21 08:41:04 +0200

    Added chat completion text and image.

---
+++ src/inference_providers/chat_completion_text_and_image.py
@@ -0,0 +1,34 @@
+import os
+from huggingface_hub import InferenceClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+client = InferenceClient(
+    provider="groq",
+    api_key=os.environ["HF_TOKEN"],
+)
+
+completion = client.chat.completions.create(
+    model="meta-llama/Llama-4-Scout-17B-16E-Instruct",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Describe this image in one sentence."
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg"
+                    }
+                }
+            ]
+        }
+    ],
+)
+
+print(completion.choices[0].message)
