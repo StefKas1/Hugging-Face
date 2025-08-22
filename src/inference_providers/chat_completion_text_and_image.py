65d062fb5e274352e08016f4542521564dbe067f
StefKas1 <stefan.kasperzack@gmail.com>
2025-08-22 09:58:52 +0200

    Formatted.

--- src/inference_providers/chat_completion_text_and_image.py
+++ src/inference_providers/chat_completion_text_and_image.py
@@ -16,17 +16,14 @@ completion = client.chat.completions.create(
         {
             "role": "user",
             "content": [
-                {
-                    "type": "text",
-                    "text": "Describe this image in one sentence."
-                },
+                {"type": "text", "text": "Describe this image in one sentence."},
                 {
                     "type": "image_url",
                     "image_url": {
                         "url": "https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg"
-                    }
-                }
-            ]
+                    },
+                },
+            ],
         }
     ],
 )
